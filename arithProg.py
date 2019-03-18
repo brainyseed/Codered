16fe42f677de93d7019415678b9072e5f28343fa
Ravi Sharma <34424293+brainyseed@users.noreply.github.com>
2019-03-18 20:09:09 +0530

    Update arithProg.py

--- arithProg.py
+++ arithProg.py
@@ -1,1 +1,9 @@
-
+#take an ap of first two integers as input
+#take another input say n now we find value in ap at nth place
+l=list(map(int,input().split()))
+a=l[0]
+b=l[1]
+d=a-b
+n=int(input())
+output=a+(n-1)*d
+print(output)
